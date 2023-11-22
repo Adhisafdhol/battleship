ea0ce96e4ab5993911361276aa00f14109469c10
Aadheuss <aadheusx@gmail.com>
2023-11-23 01:30:07 +0700

    Add player factory function with its test

---
+++ src/player.js
@@ -0,0 +1,47 @@
+const Player = (name) => {
+  const attack = (coordinates, target, fn) => {
+    target[fn](coordinates);
+  };
+
+  return {
+    get name() {
+      return name;
+    },
+    attack,
+  };
+};
+
+const AIPlayer = (name) => {
+  const findCoordinatesIndex = (coordinates, board) =>
+    board.findIndex(
+      (square) =>
+        square.coordinates[0] === coordinates[0] &&
+        square.coordinates[1] === coordinates[1],
+    );
+
+  const randomIndex = (activeSquares) =>
+    Math.random(Math.floor(activeSquares.length));
+
+  const filterBoard = (board) =>
+    board.filter((square) => square.status === "free");
+
+  const randomMove = (board) => {
+    const activeSquares = filterBoard(board);
+    const coordinates = activeSquares[randomIndex(activeSquares)];
+    return findCoordinatesIndex(coordinates, board);
+  };
+
+  const { attack } = Player(name);
+  return {
+    get name() {
+      return name;
+    },
+    attack,
+    randomMove,
+    randomIndex,
+    filterBoard,
+    findCoordinatesIndex,
+  };
+};
+
+export { Player, AIPlayer };
---
+++ src/player.test.js
@@ -0,0 +1,42 @@
+import { Player, AIPlayer } from "./player";
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+
+const player1 = Player("player1");
+const player2 = AIPlayer("player2");
+const player1Board = Gameboard(10);
+const player2Board = Gameboard(10);
+const p1ShipOne = Ship(3);
+const p2ShipOne = Ship(3);
+player1Board.placeShip([1, 1], p1ShipOne, "row");
+player2Board.placeShip([3, 1], p2ShipOne, "row");
+
+test("attack opponent board at coordinates [3, 1] hits opponent ship by 1", () => {
+  player1.attack([3, 1], player2Board, "receiveAttack");
+  expect(player2Board.getCoordinates([3, 1]).ship.numberOfHits).toBe(1);
+});
+
+test("random number will be equal and greater than 0 and less than 100", () => {
+  expect(player2.randomIndex(player1Board.board)).toBeGreaterThanOrEqual(0);
+  expect(player2.randomIndex(player1Board.board)).toBeLessThan(100);
+});
+
+test("findCoordinatesIndex([1,1]) will return 11", () => {
+  expect(player2.findCoordinatesIndex([1, 1], player1Board.board)).toBe(11);
+});
+
+test("Cannot attack at the same board twice", () => {
+  player2.attack([1, 1], player1Board, "receiveAttack");
+  player2.attack([1, 1], player1Board, "receiveAttack");
+  expect(player1Board.getCoordinates([1, 1]).ship.numberOfHits).toBe(1);
+});
+
+test("AI filtered board length will be 99 after one square was hit", () => {
+  expect(player2.filterBoard(player1Board.board).length).toBe(99);
+});
+
+test("AI filtered board length will be 98 after two different squares was hit", () => {
+  player2.attack([1, 2], player1Board, "receiveAttack");
+  player2.attack([1, 2], player1Board, "receiveAttack");
+  expect(player2.filterBoard(player1Board.board).length).toBe(98);
+});
