f405707e92b5e4201c1af36d3c05d3b09759fe9a
Aadheuss <aadheusx@gmail.com>
2023-11-24 01:15:45 +0700

    Add ship placement logics and its test
    ship placement logics require ships to be placed minimally one square
    distance from one another.

---
+++ src/placement-logic.js
@@ -0,0 +1,62 @@
+const randomIndex = (length) => Math.floor(Math.random() * length);
+
+const randomOrientation = () => {
+  const orientationList = ["row", "column"];
+  const index = randomIndex(orientationList.length);
+  return orientationList[index];
+};
+
+const isSquareOnTheSameRow = (square1, square2) => square1[0] === square2[0];
+const isIndexValid = (index) => index >= 0 && index < 100;
+
+const validateSquaresRow = (index1, index2, board) => {
+  if (isIndexValid(index1) && isIndexValid(index2)) {
+    return isSquareOnTheSameRow(
+      board[index1].coordinates,
+      board[index2].coordinates,
+    )
+      ? index1
+      : -1;
+  }
+
+  return -1;
+};
+
+const getAdjecentList = (coordinatesIndexList, board) => {
+  const list = [];
+  coordinatesIndexList.forEach((index) => {
+    list.push(validateSquaresRow(index - 11, index - 10, board));
+    list.push(index - 10);
+    list.push(validateSquaresRow(index - 9, index - 10, board));
+    list.push(validateSquaresRow(index - 1, index, board));
+    list.push(validateSquaresRow(index + 1, index, board));
+    list.push(validateSquaresRow(index + 9, index + 10, board));
+    list.push(index + 10);
+    list.push(validateSquaresRow(index + 11, index + 10, board));
+  });
+
+  return Array.from(
+    new Set(
+      list.filter((item) => item >= 0 && item < 100).sort((a, b) => a - b),
+    ),
+  );
+};
+const isPlacementValid = (placementIndex, coordinatesIndexList, board) => {
+  const shipPlacementValidity = placementIndex.every(
+    (index) => !("ship" in board[index]),
+  );
+  const adjecentListValidity = coordinatesIndexList.every(
+    (index) => !("ship" in board[index]),
+  );
+
+  return shipPlacementValidity && adjecentListValidity;
+};
+
+export {
+  randomIndex,
+  randomOrientation,
+  getAdjecentList,
+  isSquareOnTheSameRow,
+  validateSquaresRow,
+  isPlacementValid,
+};
---
+++ src/placement-logic.test.js
@@ -0,0 +1,86 @@
+import {
+  randomIndex,
+  getAdjecentList,
+  isSquareOnTheSameRow,
+  validateSquaresRow,
+  isPlacementValid,
+} from "./placement-logic";
+import Gameboard from "./gameboard";
+import Ship from "./ship";
+
+const board1 = Gameboard(10);
+
+test("Return random index that is greater or equal zero and less than its length", () => {
+  expect(randomIndex(100)).toBeGreaterThanOrEqual(0);
+  expect(randomIndex(100)).toBeLessThan(100);
+});
+
+test("isSquareOnTheSameRow(board1.board[9.]coordinates, board1.board[9].coordinates[10]) will return false", () => {
+  expect(
+    isSquareOnTheSameRow(
+      board1.board[9].coordinates,
+      board1.board[10].coordinates,
+    ),
+  ).toBe(false);
+});
+
+test("validateSquareRow(9, 10, board1.board) will return -1", () => {
+  expect(validateSquaresRow(9, 11, board1.board)).toBe(-1);
+});
+
+test("validateSquareRow(-1, 0, board1.board) will return -1", () => {
+  expect(validateSquaresRow(0, -1, board1.board)).toBe(-1);
+});
+
+test("validateSquareRow(10, 11, board1.board) will return 10", () => {
+  expect(validateSquaresRow(10, 11, board1.board)).toBe(10);
+});
+
+test("getAdjecentList([11]).length will be 8", () => {
+  expect(getAdjecentList([11], board1.board).length).toBe(8);
+});
+
+test("getAdjecentList([0], board1.board) will return [1, 10, 11]", () => {
+  expect(getAdjecentList([0], board1.board)).toEqual([1, 10, 11]);
+});
+
+test("isPlacementValid([0, 1], getAdjecentList([0, 1], board1.board), board1.board) will return true", () => {
+  expect(
+    isPlacementValid(
+      [0, 1],
+      getAdjecentList([0, 1], board1.board),
+      board1.board,
+    ),
+  ).toBeTruthy();
+});
+
+test("isPlacementValid([11, 12], getAdjecentList([11, 12], board1.board), board1.board) will return false", () => {
+  board1.placeShip([2, 1], Ship(3), "row");
+  expect(
+    isPlacementValid(
+      [11, 12],
+      getAdjecentList([11, 12], board1.board),
+      board1.board,
+    ),
+  ).toBeFalsy();
+});
+
+test("isPlacementValid([24, 34], getAdjecentList([24, 34], board1.board), board1.board) will return false", () => {
+  expect(
+    isPlacementValid(
+      [24, 34],
+      getAdjecentList([24, 34], board1.board),
+      board1.board,
+    ),
+  ).toBeFalsy();
+});
+
+test("isPlacementValid([40, 41, 42], getAdjecentList([40, 41, 42], board1.board), board1.board) will return true", () => {
+  expect(
+    isPlacementValid(
+      [40, 41, 42],
+      getAdjecentList([40, 41, 42], board1.board),
+      board1.board,
+    ),
+  ).toBeTruthy();
+});
