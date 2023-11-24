fc98e1e8b54ad98787c5a554c0bf25445284d427
Aadheuss <aadheusx@gmail.com>
2023-11-24 23:42:06 +0700

    Add function to style using random color

---
+++ src/color-changing.js
@@ -0,0 +1,17 @@
+import { randomIndex } from "./placement-logic";
+
+const setRandomColor = (colors, dom, style) => {
+  const color = colors[randomIndex(colors.length)];
+  const target = dom;
+  console.log(target, color);
+  target.style[style] = `${color}`;
+};
+
+const setMultipleBorderColor = (colors, dom) => {
+  setRandomColor(colors, dom, "border-right-color");
+  setRandomColor(colors, dom, "border-top-color");
+  setRandomColor(colors, dom, "border-bottom-color");
+  setRandomColor(colors, dom, "border-left-color");
+};
+
+export { setRandomColor, setMultipleBorderColor };
