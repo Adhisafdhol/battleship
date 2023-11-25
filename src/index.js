355133c844ce0e0f3ac83cab5e66efd07a9c93de
Aadheuss <aadheusx@gmail.com>
2023-11-25 08:58:35 +0700

    Separate gameboard creation to another file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
 import "normalize.css";
 import "./style.css";
-import {
-  setUpGameboardDom,
-  setUpPlayerBoard,
-  setUpAIBoard,
-} from "./dom-ui/dom-ui";
+import { setUpAIBoard } from "./dom-ui/dom-ui";
+import { Game } from "./dom-ui/game-dom-ui";
 
 function component() {
   const content = document.getElementById("content");
-  const gameDom = document.createElement("div");
-  gameDom.classList.add("game");
-  const vsText = document.createElement("div");
-  vsText.classList.add("vs-text");
-  vsText.textContent = "VS";
 
   const player1 = setUpAIBoard("player1");
   const player2 = setUpAIBoard("player2");
 
-  gameDom.appendChild(setUpGameboardDom(player1));
-  gameDom.appendChild(vsText);
-  gameDom.appendChild(setUpGameboardDom(player2));
-  content.appendChild(gameDom);
+  content.appendChild(Game(player1, player2));
 }
 
 component();
