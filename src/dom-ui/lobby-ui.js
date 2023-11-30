93ddbef3177de33e8709aa36dbe09be9a8664aec
Aadheuss <aadheusx@gmail.com>
2023-11-30 10:41:22 +0700

    Add that starts game on click
    the game will start by checking the lobby board and create a board that
    resemble that board as the player board.

--- src/dom-ui/lobby-ui.js
+++ src/dom-ui/lobby-ui.js
@@ -1,6 +1,9 @@
 import { createElWithClassAndText } from "./dom-method";
-import { setUpPlayerBoard, populateBoard } from "./dom-ui";
+import { setUpPlayerBoard, setUpAIBoard, populateBoard } from "./dom-ui";
 import { createGameboardTable, createShipDom } from "./ship-placement-ui";
+import { getAttributeArr } from "./drag-and-drop";
+import Ship from "../ship";
+import { Game } from "./game-dom-ui";
 
 const createGreetingDom = () => {
   const greeting = createElWithClassAndText(
@@ -12,10 +15,54 @@ const createGreetingDom = () => {
   return greeting;
 };
 
-const createStartGameBtn = () => {
-  const startGameBtn = createElWithClassAndText("button", "start-btn", "Start");
-  return startGameBtn;
+const getAllShipsData = () => {
+  const ships = [];
+  for (let i = 0; i < 10; i += 1) {
+    const shipDom = document.getElementById(`${i}`);
+    const coordinates = getAttributeArr(shipDom, "data-head");
+    const orientation = shipDom.getAttribute("data-orientation");
+    const length = Number(shipDom.getAttribute("data-length"));
+    ships.push({ coordinates, orientation, length });
+  }
+
+  return ships;
 };
+
+const removeLobby = (parentDom) => {
+  const lobby = document.querySelector(".harbor");
+  if (lobby) {
+    parentDom.removeChild(lobby);
+  }
+};
+
+const appendGameDom = (parentDom, dom) => {
+  const gameDom = document.querySelector(".game");
+  if (gameDom) {
+    parentDom.replaceChild(dom, gameDom);
+  } else {
+    parentDom.appendChild(dom);
+  }
+};
+
+const startGame = () => {
+  const content = document.getElementById("content");
+  const player1 = setUpPlayerBoard("player1");
+  const { board } = player1;
+  const ships = getAllShipsData();
+  const player2 = setUpAIBoard("AI");
+
+  ships.forEach((ship) => {
+    board.placeShip(ship.coordinates, Ship(ship.length), ship.orientation);
+  });
+
+  removeLobby(content);
+  const gameDom = Game(player1, player2);
+  appendGameDom(content, gameDom);
+};
+
+const createStartGameBtn = () =>
+  createElWithClassAndText("button", "start-btn", "Start");
+
 const createLobbyDom = () => {
   const lobby = createElWithClassAndText("div", "harbor");
   const player = setUpPlayerBoard("player1");
@@ -39,6 +86,7 @@ const createLobbyDom = () => {
     "start-btn-container",
   );
   const startGameBtn = createStartGameBtn();
+  startGameBtn.addEventListener("click", startGame);
   startBtnContainer.appendChild(startGameBtn);
   mainLobbyContainer.appendChild(greeting);
   gameboardContainer.appendChild(playerDom);
